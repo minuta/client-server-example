b9bcddcc0bd61753039cbc9d24afaefa50fd4ed4
AF <>
2019-01-10 21:08:30 +0100

    add sleep in server and extend log messaging

--- server.cc
+++ server.cc
@@ -7,33 +7,31 @@
 #include <arpa/inet.h>
 #include <unistd.h>     
 #include <pthread.h>
-#include <iostream>
 
 char client_message[2000];
 const int NUMBER_OF_THREADS = 1;    // number of possible connections
 const int BACKLOG = 10;             // max size for the queue of pending connections
 const int PORT = 7799;
 //const char* IP = "127.0.0.1";
-const char * IP = "10.0.1.1";
-const char * message = "Hello from server : got your message!";
+const char *IP = "10.0.1.1";
+const char *message = "Hello from server : got your message!";
 
 //pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
 
-void * socketThread(void *arg) {
+void *socketThread(void *arg) {
 
     int newSocket = *((int *)arg);
     recv(newSocket , client_message , 2000 , 0);
 
-    printf("got client message : %s", client_message);
+    printf("Server: got client message : %s \n", client_message);
 
     //pthread_mutex_lock(&lock);
     //pthread_mutex_unlock(&lock);
 
-
     send(newSocket, message, strlen(message), 0);
 
-    printf("Close socket and thread \n");
+    printf("Server: close socket and thread \n");
     close(newSocket);
     pthread_exit(NULL);
 }
@@ -61,32 +59,35 @@ int main(){
     bind(serverSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
 
     if ( listen(serverSocket, BACKLOG) == 0 )
-        printf("Listening... \n");
+        printf("Server: Listening... \n");
     else
-        printf("Error while trying to listen on a socket! \n");
+        printf("Server: Error while trying to listen on a socket! \n");
 
     pthread_t tid[NUMBER_OF_THREADS];
 
-    sleep(1);
+    sleep(10);
 
     for (int i=0; i<NUMBER_OF_THREADS; i++){
    
         // accept call creates a new socket for the incoming connection
         addr_size = sizeof serverStorage;
 
-        std::cout << "before accept" << std::endl;
+        //printf("Server: before syscall accept \n");
         newSocket = accept(serverSocket, (struct sockaddr *) &serverStorage, &addr_size);
-
-        std::cout << "after accept" << std::endl;
+        //printf("Server: after syscall accept \n");
+        
         //for each client request creates a thread and assign the client request to it to process
         //so the main thread can entertain next request
         if ( pthread_create(&tid[i], NULL, socketThread, &newSocket) != 0 )
-            printf("Error: failed to create thread!\n");
+            printf("Server: error: failed to create thread!\n");
     }
 
+    sleep(5);
     for (int i=0; i<NUMBER_OF_THREADS; i++){
         pthread_join ( tid[i], NULL);
     }
+    
+    printf("Server: terminating...\n");
 
     return 0;
 }
